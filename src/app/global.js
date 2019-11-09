2c371c88d679938d0b0acf5e1f23bf5cb371f224
Hiroto Kakitani <hirotokakitani@gmail.com>
2019-11-08 17:01:02 -0800

    smaller pictures

--- src/app/global.js
+++ src/app/global.js
@@ -13,10 +13,14 @@ function showContent(){
     document.querySelector(".page-content").style.display = "flex";
     document.querySelector(".page-content").style["flex-direction"] = "column";
     document.querySelector("body").style.backgroundColor = "#dedede";
-    /*if(window.location.pathname == '/src/about.html'){
-        document.querySelector("body").style.backgroundImage = 'url("../res/photos/carousel_13.JPG")'
+    if(window.location.pathname == '/src/gallery.html'){
+        let images = document.querySelectorAll("#grid > img");
+    
+        images.forEach(img => {
+            console.log(img.src);
+        });      
     }
-    */
+    
     //#fff0d6
 }
 
